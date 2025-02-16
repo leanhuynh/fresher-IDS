e461596b82f1eb2e5e9185cd42f35618baa0d760
leanhuynh <leanhuynh2002@gmail.com>
2025-02-17 00:37:08 +0700

    update validation file

--- laravel/app/Http/Request/UserRequest.php
+++ laravel/app/Http/Request/UserRequest.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Request;
+
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest {
-
+class UserRequest extends FormRequest
+{
     public function authorize()
     {
         return true; // Cho phép mọi request
@@ -13,31 +14,52 @@ class UserRequest extends FormRequest {
     public function rules()
     {
         return [
-            'role_id' => 'nullable|integer',
-            'first_name' => 'required|string|max:255,',
-            'last_name' => 'required|string|max:255,',
-            'user_name' => 'required|string|max:255|unique:App\Models\User,user_name,' . $this->route('user'),
+            'role_id' => 'nullable|integer|exists:roles,id',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'user_name' => [
+                'required', 'string', 'max:50',
+                'regex:/^[a-zA-Z0-9._]+$/',
+                'unique:App\Models\User,user_name,' . $this->route('user')
+            ],
             'email' => 'required|email|unique:App\Models\User,email,' . $this->route('user'),
             'address' => 'nullable|string|max:255',
-            'password' => 'nullable|string|min:8|max:16|confirmed',
-            'avatar' => 'nullable|sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'password' => [
+                'nullable', 'string', 'min:8', 'max:16', 'confirmed',
+                'regex:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/'
+            ],
+            'avatar' => 'nullable|sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
 
     public function messages()
     {
         return [
-            'avatar.image' => __('validation.avatar.image'),
-            'avatar.max' => __('validation.avatar.max'),
-            'name.required' => __('validation.name.required'),
-            'name.unique' => __('validation.name.unique'),
-            'name.max' => __('validation.name.max'),
+            'role_id.exists' => __('validation.role_id.exists'),
+
+            'first_name.required' => __('validation.first_name.required'),
+            'first_name.max' => __('validation.first_name.max'),
+
+            'last_name.required' => __('validation.last_name.required'),
+            'last_name.max' => __('validation.last_name.max'),
+
+            'user_name.required' => __('validation.user_name.required'),
+            'user_name.regex' => __('validation.user_name.regex'),
+            'user_name.unique' => __('validation.user_name.unique'),
+            'user_name.max' => __('validation.user_name.max'),
+
             'email.required' => __('validation.email.required'),
             'email.unique' => __('validation.email.unique'),
             'email.email' => __('validation.email.email'),
+
             'password.nullable' => __('validation.password.nullable'),
             'password.min' => __('validation.password.min'),
+            'password.max' => __('validation.password.max'),
             'password.confirmed' => __('validation.password.confirmed'),
+            'password.regex' => __('validation.password.regex'),
+
+            'avatar.image' => __('validation.avatar.image'),
+            'avatar.max' => __('validation.avatar.max'),
         ];
     }
 }
